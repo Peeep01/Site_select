d7511d1d7970be7ee38db2accf45950af707c836
Peeep01 <agrosev18@gmail.com>
2022-07-20 05:54:23 +0300

    Добавьте файлы проекта.

---
+++ Site_select/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Viborka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient webClient = new WebClient();
+            var s = webClient.DownloadString("https://samgk.ru/");
+
+            Regex regex = new Regex(@" In place of all this, write keywords (\w*)", RegexOptions.IgnoreCase);
+            MatchCollection matchess = regex.Matches(s);
+
+            if (matchess.Count > 0)
+            {
+                foreach (Match match in matchess)
+                    Console.WriteLine(match.Value);
+            }
+            else
+            {
+                Console.WriteLine("Nothing found");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
